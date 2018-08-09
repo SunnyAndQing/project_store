94858d0e7484607ee8e0ffd08835d69107841598
sunny <153515368@qq.com>
2018-08-09 20:33:58 +0800

    完成登录页

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+// 配置element-ui第三方组件库
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/css/index.css';
+// 配置element-ui
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="login-wrapper">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formData">
+      <h3>用户登录</h3>
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="formData.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+// 导入axios
+import axios from 'axios';
+export default {
+  data () {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    async handleLogin () {
+      // console.log(this.formData);
+      var response = await axios.post('http://localhost:8888/api/private/v1/login', this.formData);
+      console.log(response);
+      var {data: {meta: {msg, status}}} = response;
+      if (status === 200) {
+        // 如果登录成功
+        this.$message.success(msg);
+        // 登录成功要记录token
+        var {data: {data: {token}}} = response;
+        console.log(token, 'token');
+        sessionStorage.setItem('token', token);
+      } else {
+        this.$message.error(msg);
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.login-wrapper {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-wrapper .login-form {
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+.login-wrapper .login-form .btn-login {
+  width: 100%;
+}
+</style>
